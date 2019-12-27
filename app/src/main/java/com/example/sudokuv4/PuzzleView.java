44f8f6c2c97c96c441df3f67216ebe88ca9fb6d4
Elizabet <lia_med@mail.ru>
2019-12-27 10:50:26 +0300

    Sudokuv4

--- app/src/main/java/com/example/sudokuv4/PuzzleView.java
+++ app/src/main/java/com/example/sudokuv4/PuzzleView.java
@@ -7,6 +7,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
+
+
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
